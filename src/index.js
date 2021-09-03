7a86b55e79c5d10782379a1dda577922175dd5a6
unknown <nhanmtmt9@gmail.com>
2021-09-03 21:16:32 +0700

    sss

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const morgan = require('morgan')
+const handlebars = require('express-handlebars');
+const app = express()
+const port = 3000
+const path = require('path')
+
+app.use(express.static(path.join(__dirname,'public')))
+
+app.use(morgan('combined'))
+app.engine('hbs', handlebars({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views',path.join(__dirname,'resources/views'))
+
+console.log(path.join(__dirname,'resources/views'));
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+app.get('/news', (req, res) => {
+  res.render('news')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
